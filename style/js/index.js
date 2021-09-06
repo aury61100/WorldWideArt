a09ca5aecc266ec248c864614bd3e8e2d9ebc021
aury61100 <57524709+aury61100@users.noreply.github.com>
2021-09-06 14:32:46 +0200

    style and js

---
+++ style/js/index.js
@@ -0,0 +1,8 @@
+var animation = bodymovin.loadAnimation({
+    container: document.getElementById('wwa'),
+    rederer: 'svg',
+    loop: false,
+    autoplay: true,
+    path: 'WWA-animation.json',
+});
+
